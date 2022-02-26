d0e8a4a1630bbe1745c62e7d2bb5c41d2c0618ce
Cutler, Philip <greenthegarden@gmail.com>
2022-02-26 16:39:43 +1030

    added script to install dependencies

---
+++ install-dependencies.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+CWD="$(pwd)"
+
+install_rtl433 () {
+# install rtl_433 library
+sudo apt-get install -y libtool libusb-1.0-0-dev librtlsdr-dev rtl-sdr build-essential autoconf cmake pkg-config
+
+cd /tmp
+git clone https://github.com/switchdoclabs/rtl_433
+cd rtl_433/
+mkdir build
+cd build
+cmake ..
+make clean
+make
+sudo make install
+
+cd $CWD
+}
+
+#install_rtl433
+
+install_mqsql () {
+# install mysql and create database
+sudo apt-get install -y python3-dev python3-pip mariadb-server
+sudo python3 -m pip install mysqlclient
+
+sudo mysql -u root -p < WeatherSenseWireless.sql
+}
+
+# install python dependencies
+sudo apt-get install -y libopenjp2-7
+sudo python3 -m pip install -r requirements.txt
+
