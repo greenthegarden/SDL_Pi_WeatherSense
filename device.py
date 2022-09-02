68fe5d0919afed05efa416874d090e1d6875102d
Cutler, Philip <greenthegarden@gmail.com>
2022-09-02 23:34:55 +0930

    test hass mqtt discovery support

--- device.py
+++ device.py
@@ -1,23 +1,17 @@
-#  Copyright (c) 2022 - Andreas Philipp
-#  This code is published under the MIT license
-
 # (c) Andreas Philipp - 2021
 #  andreas.philipp@anphi.de
 
-# simple demo script for a MQTT controlled switch
-# The device registers itself in homeassistant via the homeassistant
-# MQTT discovery protocol and shows up as switch
-# when the device is switched on or off the accordingly callbacks are called
-# on() and off() in this example
-
 
 import time
+from random import uniform
 
 from paho.mqtt.client import Client
 
 from ha_mqtt.ha_device import HaDevice
-from ha_mqtt.mqtt_device_base import MqttDeviceSettings
-from ha_mqtt.mqtt_switch import MqttSwitch
+from ha_mqtt.mqtt_device_base import MqttDeviceBase, MqttDeviceSettings
+from ha_mqtt.mqtt_sensor import MqttSensor
+from ha_mqtt.mqtt_thermometer import MqttThermometer
+from ha_mqtt.util import HaDeviceClass
 
 # instantiate an paho mqtt client and connect to the mqtt server
 client = Client("testscript")
@@ -25,15 +19,29 @@ client.connect("localhost", 1883)
 client.loop_start()
 
 
-# create device info dictionary
-dev = HaDevice("Testdevice", "test123456-veryunique")
-dev.add_config_option(settings: MqttDeviceSettings, unit: str="°C", send_only=True)
+dev = HaDevice("FT16", "Indoor Sensor 1")
+dev_hum = MqttDeviceSettings("FT16_1_hum", "hum1", client)
+dev_temp = MqttDeviceSettings("FT16_1_temp", "temp1", client)
+sens_hum = MqttSensor(dev_hum, "%", HaDeviceClass.HUMIDITY)
+sens_temp = MqttSensor(dev_temp, "°C", HaDeviceClass.TEMPERATURE)
+# instantiate an MQTTThermometer object
+# th = MqttThermometer(dev_set, "°C")
+
 try:
     while True:
-        time.sleep(0.1)
+        # publish a random "temperature" every 5 seconds
+        temp = f"{uniform(-10, 10):2.2f}"
+        print(
+            f"publishing temperature: {temp} {sens_temp.unit_of_measurement}")
+        sens_temp.publish_state(temp)
+        sens_hum.publish_state(34)
+        time.sleep(5)
+
 except KeyboardInterrupt:
     pass
 finally:
     # close the device for cleanup. Gets marked as offline/unavailable in homeassistant
+    sens_hum.close()
+    sens_temp.close()
     client.loop_stop()
     client.disconnect()
